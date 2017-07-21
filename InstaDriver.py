72e038f596a195ad6a6d761e186e53d2a5721cff
Philipp Matthes <philippmatthes@Philipps-MacBook-Pro.local>
2017-07-22 01:33:26 +0200

    Add Comments to increase readability

--- InstaDriver.py
+++ InstaDriver.py
@@ -38,17 +38,26 @@ class Tell:
 
 class Driver(object):
     def __init__(self):
+
+        # Set up Telegram Message Client
         self.mailer = Mailer()
+
+        # Set up virtual display for Raspberry Pi compatibility
         self.display = Xvfb()
         self.display.start()
         self.logpath = sys.path[0]+"/console.txt"
         open(self.logpath, 'w').close()
+
+        # Load history
         with open("followed_users_all_time.txt","rb") as userfile_all_time:
             self.followed_accounts = pickle.load(userfile_all_time)
         with open("followed_users.txt","rb") as userfile:
             self.accounts_to_unfollow = pickle.load(userfile)
+
         # The following (xpath) classes need to be refreshed every now and then.
-        # self.login_class = "_71bsb"
+        # they define, where elements are located on Instagram. Sometimes,
+        # classes are Changed (eg. "coreSpriteHeartOpen" to "coreSpriteLikeHeartOpen")
+        # this results in vast amount of errors and needs to be corrected.
         self.first_ele_class = "_ovg3g"
         self.sections_xpath = "//*[contains(@class, '_6jvgy')]"
         self.local_name_xpath = ".//a[@class='_4zhc5 notranslate _j7lfh']"
@@ -63,9 +72,15 @@ class Driver(object):
         self.next_button_xpath = "//*[contains(@class, '_de018 coreSpriteRightPaginationArrow')]"
         self.like_button_xpath = "//*[contains(@class, '_soakw coreSpriteHeartOpen')]"
         self.like_button_full_xpath = "//*[contains(@class, '_soakw coreSpriteHeartFull')]"
+
+        # Get username and password for logging in
         self.username = input("Username?")
         self.password = input("Password?")
+
+        # Clearing the command line
         os.system('clear')
+
+        # Final setup
         self.topics = ["graphic","render","cartoon","daily","art","design","cinema4d","animation","cg","illustration"]
         self.delay = 30
         self.start_url = "https://www.instagram.com/accounts/login/"
@@ -73,15 +88,18 @@ class Driver(object):
         self.browser = webdriver.PhantomJS()
         self.browser.set_window_size(1980,1080)
 
+    # Checks if a user was followed already
     def user_followed_already(self, user):
         if user in self.followed_accounts:
             return True
         else:
             return False
 
+    # Returns nicely formatted timestamp
     def timestamp(self):
         return time.strftime('%a %H:%M:%S')+" "
 
+    # Logs into Instagram automatically
     def login(self):
         self.mailer.send(self.timestamp()+"Logging in.")
         print(self.timestamp()+"Logging in.")
@@ -106,6 +124,7 @@ class Driver(object):
             self.login()
             return
 
+    # Comments on a picture
     def comment(self):
         sleep(3)
         query = Tell.comment[randint(0,len(Tell.comment)-1)]
@@ -123,11 +142,14 @@ class Driver(object):
             self.mailer.send(self.timestamp()+"Comment field not found.\n")
             print(self.timestamp()+"Comment field not found.")
 
+    # Searches for a certain topic
     def search(self, query):
         self.mailer.send(self.timestamp()+"Searching for "+query+".")
         print(self.timestamp()+"Searching for "+query+".")
         self.browser.get("https://www.instagram.com/explore/tags/"+query+"/")
 
+    # Checks for error which occurs when pictures are removed while
+    # switching through
     def error(self):
         try:
             error_message = self.browser.find_element_by_xpath(self.error_xpath)
@@ -139,6 +161,7 @@ class Driver(object):
         except:
             return False
 
+    # Selects the first picture in a loaded topic screen
     def select_first(self):
         try:
             first_picture = self.browser.find_element_by_class_name(self.first_ele_class)
@@ -152,6 +175,7 @@ class Driver(object):
             #     self.select_first()
             return False
 
+    # Switches to the next picture
     def next_picture(self):
         try:
             sleep(1)
@@ -167,10 +191,12 @@ class Driver(object):
                 self.select_first()
             return
 
+    # Skips recommended pictures
     def skip_recommended(self):
         for x in range(9):
             self.next_picture()
 
+    # Loads the authors name
     def author(self):
         try:
             author = self.browser.find_element_by_xpath(self.author_xpath)
@@ -182,6 +208,7 @@ class Driver(object):
             print(self.timestamp()+"Author xpath not found.")
             return ""
 
+    # Checks if the post is already liked
     def already_liked(self):
         try:
             full = self.browser.find_element_by_xpath(self.like_button_full_xpath)
@@ -189,6 +216,7 @@ class Driver(object):
         except:
             return False
 
+    # Likes a picture
     def like(self, topic):
         count = 0
         while self.already_liked() and count < 10:
@@ -215,6 +243,7 @@ class Driver(object):
             self.like(topic)
             return
 
+    # Depreciated
     def open_unfollow_screen(self):
         self.browser.get(self.account_url)
         sleep(2)
@@ -225,6 +254,7 @@ class Driver(object):
                 element.click()
         sleep(2)
 
+    # Depreciated
     def unfollow_via_unfollow_screen(self):
         try:
             sections = self.browser.find_elements_by_xpath(self.sections_xpath)
@@ -239,6 +269,7 @@ class Driver(object):
             sleep(10)
         return
 
+    # Unfollows a user
     def unfollow(self, name):
         self.browser.get("https://www.instagram.com/"+name+"/")
         sleep(3)
@@ -255,6 +286,7 @@ class Driver(object):
             print(self.timestamp()+"Unfollow button not found.")
             sleep(1)
 
+    # Follows a user
     def follow(self):
         sleep(3)
         try:
@@ -275,13 +307,15 @@ class Driver(object):
             print(self.timestamp()+"Follow button not found.")
             sleep(1)
 
-    # def unfollow_all(self):
-    #     self.open_unfollow_screen()
-    #     for name in self.accounts_to_unfollow:
-    #         self.unfollow(name)
-    #         sleep(10)
-    #     del self.accounts_to_unfollow[:]
+    # Depreciated
+    def unfollow_all(self):
+        self.open_unfollow_screen()
+        for name in self.accounts_to_unfollow:
+            self.unfollow(name)
+            sleep(10)
+        del self.accounts_to_unfollow[:]
 
+    # Coordinates every function in an endless loop
     def like_follow_loop(self):
         self.login()
         while True:
